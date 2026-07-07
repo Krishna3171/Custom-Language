8c999018d43948789b4e5374965c06cb91c08a44
Vasudev <krishnaagarwal2905@gmail.com>
2026-07-07 14:31:23 +0530

    adding parser logic

--- Include/Parser.h
+++ Include/Parser.h
@@ -9,13 +9,28 @@
 class Parser
 {
 public:
-    explicit Parser(const std::vector<Token>& tokens);
-
-    
+    explicit Parser(const std::vector<Token> &tokens);
 
     Program parse();
 
+
 private:
-    const std::vector<Token>& tokens;
+    const std::vector<Token> &tokens;
     size_t current = 0;
+
+    bool isAtEnd() const;
+
+    Token peek() const;
+
+    Token previous() const;
+
+    Token advance();
+
+    bool check(TokenType type) const;
+
+    bool match(TokenType type);
+
+    Token consume(TokenType type, const std::string &message);
+
+    
 };
---
+++ Src/Parser.cpp
@@ -0,0 +1,9 @@
+#include "D:\computer science\Custom Language\Include/Parser.h"
+
+#include <iostream>
+
+Parser::Parser(const std::vector<Token> &tokens)
+    : tokens(tokens)
+{
+    
+}
